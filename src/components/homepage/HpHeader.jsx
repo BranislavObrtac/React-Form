3200bea23bd2f2668a120d1b832f2da5527f4c29
Branislav Obrtáč <brano.obrtac@gmail.com>
2022-10-12 14:56:31 +0200

    Pridaný základ ku vyhľadávaniu

--- src/components/homepage/HpHeader.jsx
+++ src/components/homepage/HpHeader.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./HpHeader.module.scss";
-import SearchIcon from "@mui/icons-material/Search";
+import HpSearch from "./HpSearch";
+import Data from "../../Data.json";
 
 function HpHeader() {
   return (
     <div className={styles["hp-header"]}>
       <h3 className={styles["hp-header-title"]}>Hľadajte na enviroportáli</h3>
-      <input type="text" name="search" placeholder="Hladat" />
+      <div className={styles["bg-image"]}>
+        <HpSearch placeholder={"Zadajte hľadaný výraz..."} data={Data} />
+      </div>
     </div>
   );
 }
