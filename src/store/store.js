bc5a3c75b88068f78bf318bff3b9a8a03bc20f60
Branislav Obrtáč <brano.obrtac@gmail.com>
2022-11-09 12:43:32 +0100

    Pridaný mainMenu slice a vymazané už nepotrebné slices

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import navSlice from "./navSlice";
-import menuItemsSlice from "./menuSlice";
 import searchSlice from "./searchSlice";
 import tabSlice from "./tabSlice";
+import mainMenuSlice from "./mainMenuSlice";
 
 const store = configureStore({
   reducer: {
-    nav: navSlice.reducer,
-    menuItems: menuItemsSlice.reducer,
     searchData: searchSlice.reducer,
     tabs: tabSlice.reducer,
+    mainMenu: mainMenuSlice.reducer,
   },
 });
-export const navActions = navSlice.actions;
 export default store;
