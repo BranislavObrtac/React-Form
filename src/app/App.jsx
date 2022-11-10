00663546d5c16ce6c233802bd0b62c106f860c55
Branislav Obrtáč <brano.obrtac@gmail.com>
2022-11-10 14:04:50 +0100

    Pridaný codeSplitting (lazy,suspense)

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,24 +1,31 @@
 import "./App.scss";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import Footer from "../components/footer/Footer";
-import Odpady from "../components/pages/odpady/Odpady";
-import HomePage from "../components/pages/homepage/HomePage";
-import NotFound from "../components/pages/notfound/NotFound";
+
+//components
 import MainMenu from "../components/mainmenu/MainMenu";
+import HomePage from "../components/pages/homepage/HomePage";
+
+//lazy load => CODE SPLITTING
+const Odpady = lazy(() => import("../components/pages/odpady/Odpady"));
+const Footer = lazy(() => import("../components/footer/Footer"));
+const NotFound = lazy(() => import("../components/pages/notfound/NotFound"));
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route element={<MainMenu />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="odpady" element={<Odpady />}>
-            <Route path=":id" element={<Odpady />} />
+      <Suspense fallback={<h5>Načítavanie...</h5>}>
+        <Routes>
+          <Route element={<MainMenu />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="odpady" element={<Odpady />}>
+              <Route path=":id" element={<Odpady />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
           </Route>
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-      <Footer />
+        </Routes>
+        <Footer />
+      </Suspense>
     </>
   );
 }
