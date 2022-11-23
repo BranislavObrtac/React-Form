1da55321631f925f044d188b91888d634b3a5c7d
Branislav Obrtáč <brano.obrtac@gmail.com>
2022-11-23 09:37:36 +0100

    Upravene App loaduje footer

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,29 +1,22 @@
 import "./App.scss";
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 //components
 import MainMenu from "../components/mainmenu/MainMenu";
 import Page from "../components/pages/Page";
-
-//lazy load => CODE SPLITTING
-//const Footer = lazy(() => import("../components/footer/Footer"));
-const NotFound = lazy(() => import("../components/pages/notfound/NotFound"));
+import NotFound from "../components/pages/notfound/NotFound";
+import Footer from "../components/footer/Footer";
 
 function App() {
   return (
     <>
-      <Suspense fallback={<h5>Načítavanie...</h5>}>
-        <MainMenu />
-        <Routes>
-          {/* <Route path="odpady" element={<Odpady />}>
-            <Route path=":id" element={<Odpady />} />
-          </Route> */}
-          <Route path="404" element={<NotFound />} />
-          <Route path="*" element={<Page />} />
-        </Routes>
-        {/* <Footer /> */}
-      </Suspense>
+      <MainMenu />
+      <Routes>
+        <Route path="404" element={<NotFound />} />
+        <Route path="*" element={<Page />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
