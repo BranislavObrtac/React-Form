e4c1e23b3a73913860d2d80aca0a592e7e0b6795
Branislav Obrtáč <brano.obrtac@gmail.com>
2022-11-25 09:40:42 +0100

    Upravene CSS footer max width 1440px

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,35 +7,37 @@ function Footer(link) {
   const fetchPageDataIsSuccess = useSelector(pageDataIsSuccess);
 
   return fetchPageDataIsSuccess ? (
-    <footer className={styles.footer}>
-      <section className={styles["footer-left"]}>
-        <div className={styles["footer-left-links"]}>
-          <a href={link}>Kontakt</a>
-          <a href={link}>Rss</a>
-          <a href={link}>Mapa stránok</a>
-          <a href={link}>Cookies</a>
-          <a href={link}>Vyhlásenie o prístupnosti</a>
-        </div>
-        <a href={link}>
-          <b>Našli ste na stránke chybu ?</b>
-        </a>
-      </section>
-      <section className={styles["footer-right"]}>
-        <div className={styles["footer-right-logo"]}>
-          <img
-            src="https://bratislavafotogenicka.eu/wp-content/uploads/2016/12/enviroportal-logo-1.jpg"
-            alt="enviroportal logo"
-          />
-          <div>
-            Prevádzku stránky <a href={link}>www.enviroportal.sk</a> a správu
-            jej obsahu zabezpečuje{" "}
-            <a href={link}>Slovenská agentúra životného prostredia.</a>
+    <footer className={styles["footer-wrapper"]}>
+      <div className={styles["footer"]}>
+        <section className={styles["footer-left"]}>
+          <div className={styles["footer-left-links"]}>
+            <a href={link}>Kontakt</a>
+            <a href={link}>Rss</a>
+            <a href={link}>Mapa stránok</a>
+            <a href={link}>Cookies</a>
+            <a href={link}>Vyhlásenie o prístupnosti</a>
           </div>
-        </div>
-        <p>
-          ©2005–2022 <a href="/">www.enviroportal.sk</a>
-        </p>
-      </section>
+          <a href={link}>
+            <b>Našli ste na stránke chybu ?</b>
+          </a>
+        </section>
+        <section className={styles["footer-right"]}>
+          <div className={styles["footer-right-logo"]}>
+            <img
+              src="https://bratislavafotogenicka.eu/wp-content/uploads/2016/12/enviroportal-logo-1.jpg"
+              alt="enviroportal logo"
+            />
+            <div>
+              Prevádzku stránky <a href={link}>www.enviroportal.sk</a> a správu
+              jej obsahu zabezpečuje{" "}
+              <a href={link}>Slovenská agentúra životného prostredia.</a>
+            </div>
+          </div>
+          <p>
+            ©2005–2022 <a href="/">www.enviroportal.sk</a>
+          </p>
+        </section>
+      </div>
     </footer>
   ) : null;
 }
