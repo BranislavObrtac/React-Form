54babb127a86689a8ca1d1d01c1322ab62b77cf1
Branislav Obrtáč <brano.obrtac@gmail.com>
2022-11-25 09:18:59 +0100

    Pridaný page background image component

---
+++ src/components/pageblocks/PageBGImage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./PageBGImage.module.scss";
+
+const PageBGImage = () => {
+  return <div className={styles["page-background-image"]}></div>;
+};
+
+export default PageBGImage;
