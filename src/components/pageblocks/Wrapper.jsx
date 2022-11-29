a5301e24c295891d5ca0fec9b505fcb7bdd37bd7
Branislav Obrtáč <brano.obrtac@gmail.com>
2022-11-29 11:02:38 +0100

    zmazana bodkociarka mimo tagu

--- src/components/pageblocks/Wrapper.jsx
+++ src/components/pageblocks/Wrapper.jsx
@@ -2,17 +2,7 @@ import React from "react";
 import styles from "./Wrapper.module.scss";
 
 const Wrapper = (props) => {
-  return (
-    <>
-      {/* <div
-        style={{
-          backgroundImage: `url("https://cdn.pixabay.com/photo/2014/09/14/18/04/dandelion-445228_960_720.jpg")`,
-        }}
-        className={styles["wrapper-bgimage"]}
-      ></div> */}
-      <div className={styles["wrapper"]}>{props.children}</div>;
-    </>
-  );
+  return <div className={styles["wrapper"]}>{props.children}</div>;
 };
 
 export default Wrapper;
